360beadd5f8a27e38838bab7e0c4c5192a283249
Cristian Rojas <crisdarroca@hotmail.com>
2023-04-30 13:56:31 -0500

    feat: fixed menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Menu } from "../Menu/Menu";
 import styles from "./header.module.scss";
 
 const Header = () => {
+  const [fixed, setFixed] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => setFixed(window.scrollY > 0));
+  }, []);
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${fixed ? styles.fixed : ""}`}>
       <div className={styles.header_container}>
         <Link className={styles.logo} href="/">
           CR
