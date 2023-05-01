e260dd7f1f3dec835ed49c98c7793f4f6078abf3
Cristian Rojas <crisdarroca@hotmail.com>
2023-04-30 21:39:05 -0500

    feat: add logos background component

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,3 +1,10 @@
+import { ReactIcon } from "../svg/ReactIcon";
+import { CssIcon } from "../svg/CssIcon";
+import { GitIcon } from "../svg/GitIcon";
+import { HtmlIcon } from "../svg/HtmlIcon";
+import { JavaScriptIcon } from "../svg/JavaScriptIcon";
+import { TypeScriptIcon } from "../svg/TypeScriptIcon";
+import { NodeIcon } from "../svg/NodeIcon";
 import styles from "./background.module.scss";
 
 const GenerateRandomStyles = () => {
@@ -10,18 +17,36 @@ const GenerateRandomStyles = () => {
     animationDuration: `${Math.floor(Math.random() * 20) + 5}s`,
   };
 
-  const widthHeight = `${Math.floor(Math.random() * 20)}s`;
+  const widthHeight = `${Math.floor(Math.random() * 100)}px`;
   styles.height = widthHeight;
   styles.width = widthHeight;
   return styles;
 };
 
+const Icons = [
+  ReactIcon,
+  CssIcon,
+  NodeIcon,
+  GitIcon,
+  HtmlIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  TypeScriptIcon,
+  NodeIcon,
+  ReactIcon,
+];
+
 const Background = () => {
   return (
     <div className={styles.background}>
-      {new Array(10).fill("").map((_, i) => (
-        <span key={i} style={GenerateRandomStyles()}></span>
-      ))}
+      {new Array(10).fill("").map((_, i) => {
+        const Icon = Icons[i];
+        return (
+          <span key={i} style={GenerateRandomStyles()}>
+            <Icon />
+          </span>
+        );
+      })}
     </div>
   );
 };
